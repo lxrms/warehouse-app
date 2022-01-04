0683df6781757b346926c286b87ea8ae84e1b337
Alex Ramos <oemaildoalexr@gmail.com>
2022-01-03 23:22:05 -0300

    Usuario ve galpoes na tela inicial

---
+++ spec/system/visit_homepage_spec.rb
@@ -0,0 +1,31 @@
+require 'rails_helper'
+
+describe 'Visitor opens homepage' do
+  it 'and sees a welcome message' do
+    visit root_path
+    # expect(page).to have_content 'Boas vindas ao WareHouse App'
+    expect(page).to have_css 'h1', text: 'Boas vindas ao WareHouse App'
+  end
+
+  it 'and sees registered warehouses' do
+    # Arrange
+    Warehouse.new(name: 'Guarulhos', code: 'GRU').save
+    Warehouse.new(name: 'Porto Alegre', code: 'POA').save
+    Warehouse.new(name: 'São Luís', code: 'SLZ').save
+    Warehouse.new(name: 'Vitória', code: 'VIX').save
+    
+    # Act
+    visit root_path
+    
+    # Assert
+    expect(page).to have_content 'Guarulhos'
+    expect(page).to have_content 'GRU'
+    expect(page).to have_content 'Porto Alegre'
+    expect(page).to have_content 'POA'
+    expect(page).to have_content 'São Luís'
+    expect(page).to have_content 'SLZ'
+    expect(page).to have_content 'Vitória'
+    expect(page).to have_content 'VIX'
+
+  end
+end
