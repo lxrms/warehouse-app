60c154a6527fd6025bcede180dd72fc0872928f1
Alex Ramos <oemaildoalexr@gmail.com>
2022-01-04 23:49:14 -0300

    Cadastro de novos warehouses

--- app/controllers/warehouses_controller.rb
+++ app/controllers/warehouses_controller.rb
@@ -6,4 +6,22 @@ class WarehousesController < ApplicationController
   def new
     @warehouse = Warehouse.new
   end
+
+  def create
+    warehouse = Warehouse.new(warehouse_params)
+    if warehouse.save
+      redirect_to warehouse, notice: 'Galpão registrado com sucesso'
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def warehouse_params
+    params.require(:warehouse).permit(:name, :code, :description,
+                                      :address, :city, :state,
+                                      :postal_code, :total_area,
+                                      :useful_area)
+  end
 end
--- spec/system/register_warehouse_spec.rb
+++ spec/system/register_warehouse_spec.rb
@@ -21,4 +21,33 @@ describe 'Visitor registers a warehouse' do
     expect(page).to have_field 'Área Útil'
     expect(page).to have_button 'Gravar'
   end
+
+  it 'successfully' do
+    # Arrange
+
+    # Act
+    visit root_path
+    click_on 'Cadastrar novo galpão'
+    fill_in 'Nome', with: 'Juiz de Fora'
+    fill_in 'Código', with: 'JDF'
+    fill_in 'Endereço', with: 'Av Rio Branco'
+    fill_in 'Cidade', with: 'Juiz de Fora'
+    fill_in 'Estado', with: 'MG'
+    fill_in 'CEP', with: '36000-000'
+    fill_in 'Descrição', with: 'Um galpão mineiro com o pé no Rio'
+    fill_in 'Área Total', with: '5000'
+    fill_in 'Área Útil', with: '3000'
+    click_on 'Gravar'
+
+    # Assert
+    expect(page).to have_content 'Juiz de Fora'
+    expect(page).to have_content 'JDF'
+    expect(page).to have_content 'Um galpão mineiro com o pé no Rio'
+    expect(page).to have_content 'Av Rio Branco'
+    expect(page).to have_content 'Juiz de Fora/MG'
+    expect(page).to have_content 'CEP: 36000-000'
+    expect(page).to have_content 'Área Total: 5000 m²'
+    expect(page).to have_content 'Área Útil: 3000 m²'
+    expect(page).to have_content 'Galpão registrado com sucesso'
+  end
 end
