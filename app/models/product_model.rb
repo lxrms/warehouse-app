9e224b2aaf9f32e467bfe40436a2b8a01c5ecf01
Alex Ramos <oemaildoalexr@gmail.com>
2022-01-08 23:36:09 -0300

    Formata navegação e conserta i18n do Devise

--- app/models/product_model.rb
+++ app/models/product_model.rb
@@ -1,7 +1,19 @@
 class ProductModel < ApplicationRecord
-  belongs_to :supplier
+  belongs_to :supplier 
+
+  # before_create :generate_sku
 
   def dimensions
     "#{height} x #{width} x #{length}"
   end
+
+  private
+  
+  # def generate_sku
+  #   new_sku = SecureRandom.hex(14).upcase
+  #   until ProductModel.find_by(sku: new_sku).nil? do
+  #     new_sku = SecureRandom.hex(14).upcase
+  #   end
+  #   self.sku = new_sku
+  # end
 end
--- spec/system/user_sign_in_spec.rb
+++ spec/system/user_sign_in_spec.rb
@@ -1,14 +1,14 @@
 require 'rails_helper'
 
 describe 'User logs in' do
-  it 'com sucesso' do
+  it 'successfully' do
     # Arrange
     User.create! email: 'alex@gmail.com', password: '12345678'
 
     # Act
     visit root_path
     click_on 'Login'
-    fill_in 'Email', with: 'alex@gmail.com'
+    fill_in 'E-mail', with: 'alex@gmail.com'
     fill_in 'Senha', with: '12345678'
     click_on 'Entrar'
 
@@ -17,5 +17,26 @@ describe 'User logs in' do
     expect(page).to have_link 'Sair'
     expect(page).not_to have_link 'Entrar'
     expect(page).to have_content 'Olá, alex@gmail.com'
+    expect(page).to have_content 'Login efetuado com sucesso.'
+  end
+
+  it 'and logs out' do
+    # Arrange
+    User.create! email: 'alex@gmail.com', password: '12345678'
+
+    # Act
+    visit root_path
+    click_on 'Login'
+    fill_in 'E-mail', with: 'alex@gmail.com'
+    fill_in 'Senha', with: '12345678'
+    click_on 'Entrar'
+    click_on 'Sair'
+
+    # Assert
+    expect(current_path).to eq root_path
+    expect(page).to have_link 'Login'
+    expect(page).not_to have_link 'Sair'
+    expect(page).not_to have_content 'Olá, alex@gmail.com'
+    expect(page).to have_content 'Logout efetuado com sucesso.'
   end
 end
