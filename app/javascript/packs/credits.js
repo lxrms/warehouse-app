db5e8bad56ff264675866caa22288ce1e5e2d35c
Alex Ramos <oemaildoalexr@gmail.com>
2022-02-14 16:37:38 -0300

    Adiciona creditos ao logo

---
+++ app/javascript/packs/credits.js
@@ -0,0 +1,12 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const logo = document.querySelector("#logo")
+  logo.addEventListener('click', (e) => {
+    if (e.altKey) {
+      alert('Criado por Alex Ramos')
+    } else {
+      window.location.href = "/";
+
+    }
+
+  })
+})
